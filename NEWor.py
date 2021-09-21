ec977f9ed50ca02b7701bb4dc0a878c8909c34b3
Your Namesharath957 <sharath957@gmail.com>
2021-09-21 17:17:44 +0530

    Final update

---
+++ NEWor.py
@@ -0,0 +1,42 @@
+"""
+Author: sharath
+email: python
+"""
+#from utils.model import Perceptron
+from oneNeuron.perceptron import Perceptron
+from utils.all_utils import prepare_data,save_plot,save_model
+import pandas as pd 
+import numpy as np
+import logging
+import os
+
+logging_str = "[%(asctime)s: %(levelname)s:%(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,'running_logs'),level=logging.INFO, format=logging_str,filemode='a')
+
+
+OR = {
+    "x1": [0,0,1,1],
+    "x2": [0,1,0,1],
+    "y": [0,1,1,1],
+}
+
+df = pd.DataFrame(OR)
+
+logging.info(f"This is actual dataframe{df}")
+ 
+
+X,y = prepare_data(df)
+
+ETA = 0.3 # 0 and 1
+EPOCHS = 10
+model = Perceptron(eta=ETA, epochs=EPOCHS)
+model.fit(X, y)
+
+_ = model.total_loss()
+
+save_model(model,filename="or.model") 
+save_plot(df,'or.png',model)
+
+
