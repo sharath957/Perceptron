61f9a72b379f44a04e2451198e1200b9ed6c4e70
Your Namesharath957 <sharath957@gmail.com>
2021-09-21 11:25:48 +0530

    logs recording

--- AND.py
+++ AND.py
@@ -2,6 +2,13 @@ from utils.model import Perceptron
 from utils.all_utils import prepare_data,save_plot,save_model
 import pandas as pd 
 import numpy as np
+import logging
+import os
+
+logging_str = "[%(asctime)s: %(levelname)s:%(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,'running_logs'),level=logging.INFO, format=logging_str,filemode='a')
 
 
 
@@ -13,9 +20,7 @@ AND = {
 
 df = pd.DataFrame(AND)
 
-
-
-print(df) 
+logging.info(f"This is actual dataframe{df}")
 
 
 X,y = prepare_data(df)
--- OR.py
+++ OR.py
@@ -12,8 +12,7 @@ import os
 logging_str = "[%(asctime)s: %(levelname)s:%(module)s] %(message)s"
 log_dir = "logs"
 os.makedirs(log_dir,exist_ok=True)
-logging.basicConfig(filename=os.path.join(log_dir,'running_logs.log'),level=logging.INFO, format=logging_str)
-
+logging.basicConfig(filename=os.path.join(log_dir,'running_logs'),level=logging.INFO, format=logging_str,filemode='a')
 
 
 OR = {
